a9b587bb70bfb55f1b3282890ad272cd88681052
mtsatinyan <m.tsatinyan@digitai.group>
2024-06-03 20:34:50 +0400

    change the url in order to post images to the backend

--- src/api/useUploadPic.ts
+++ src/api/useUploadPic.ts
@@ -8,15 +8,11 @@ const uploadPic = async (values: TValues) => {
   const formData = new FormData();
   formData.append("file", values.file!);
   try {
-    const response = await axios.post(
-      "http://localhost:3001/self/upload",
-      formData,
-      {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      }
-    );
+    const response = await axios.post("http://localhost:3001/image", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
     console.log("File uploaded successfully", response.data);
   } catch (error) {
     console.error("Error uploading file", error);
